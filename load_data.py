0915a0a0b30433a626e93af487017e47ea4e74f0
Ryan Rao <raor2@rpi.edu>
2019-03-31 16:18:18 -0400

    commit

--- load_data.py
+++ load_data.py
@@ -19,9 +19,11 @@ def setupDatabase():
 
 crimeFile = open('crimeData.csv')
 employmentFile = open('employmentData.csv')
+countyFile = open('Area_remap.csv')
 
 crimeReader = csv.reader(crimeFile)
 employmentReader = csv.reader(employmentFile)
+countyReader = csv.reader(countyFile)
 
 crimeArray = []
 
@@ -31,23 +33,16 @@ areaTypeLookupAdded = []
 NAICSLookupAdded = []
 wagesAdded = []
 crimeAdded = []
-<<<<<<< Updated upstream
-
-=======
-
-
-
-
 
 connection_string = "host='localhost' dbname='finalproject' user='finalproject' password='finalproject'"
 conn = psycopg2.connect(connection_string,cursor_factory=psycopg2.extras.DictCursor)
 setupDatabase()
 
-
+##############TABLE 3######################
 for row in employmentReader:
     with conn.cursor() as cursor:
         if(row[1] != 'Area' and not (row[1],row[0]) in areaTypeLookupAdded): 
-            q1 = "INSERT INTO areatypelookup VALUES('" + row[1] + "' , '" + row[0] + "');"   # WHERE " + row[1] + " NOT IN areatypelookup;"
+            q1 = "INSERT INTO areatypelookup VALUES('" + row[1] + "' , '" + row[0] + "');"
             print(q1)
            # print(areaTypeLookupAdded)
             cursor.execute(q1)
@@ -55,42 +50,38 @@ for row in employmentReader:
         
             areaTypeLookupAdded.append((row[1],row[0]))
 
-for row in crimeReader:
-    with conn.cursor() as cursor:
-        query = "INSERT INTO countylookup VALUES(" + "'" + row[0] + "' , '" + row[1] + "');"
-        cursor.execute(query)
-        conn.commit()
 
+#############TABLE 4######################            
+employmentFile.close()
+employmentFile = open('employmentData.csv')
+employmentReader = csv.reader(employmentFile)
 
-#print(crimeArray[1][1])
->>>>>>> Stashed changes
-
-#for row in employmentReader:
+for row in employmentReader:
     #print(row)
-
-
-
-connection_string = "host='localhost' dbname='finalproject' user='finalproject' password='finalproject'"
-conn = psycopg2.connect(connection_string,cursor_factory=psycopg2.extras.DictCursor)
-setupDatabase()
-
-
-for row in employmentReader:
     with conn.cursor() as cursor:
-        if(row[1] != 'Area' and not (row[1],row[0]) in areaTypeLookupAdded): 
-            q1 = "INSERT INTO areatypelookup VALUES('" + row[1] + "' , '" + row[0] + "');"   # WHERE " + row[1] + " NOT IN areatypelookup;"
-            print(q1)
-           # print(areaTypeLookupAdded)
-            cursor.execute(q1)
+        row[3] = row[3].replace("'","")
+
+        if(row[2] != 'NAICS' and not (row[2],row[3]) in NAICSLookupAdded):
+            q2 = "INSERT INTO naicslookup VALUES('" + row[2] + "', '" + row[3] + "');"
+            print(q2)
+            cursor.execute(q2)
             conn.commit()
-        
-            areaTypeLookupAdded.append((row[1],row[0]))
+            NAICSLookupAdded.append((row[2],row[3]))
 
-for row in crimeReader:
-    with conn.cursor() as cursor:
-        query = "INSERT INTO countylookup VALUES(" + "'" + row[0] + "' , '" + row[1] + "');"
-        cursor.execute(query)
-        conn.commit()
+###############TABLE 5#######################
+employmentFile.close()
+employmentFile = open('employmentData.csv')
+employmentReader = csv.reader(employmentFile)
+
+
+
+
+#print("Adding from Crimes File")
+#for row in crimeReader:
+#    with conn.cursor() as cursor:
+#        query = "INSERT INTO countylookup VALUES(" + "'" + row[0] + "' , '" + row[1] + "');"
+#        cursor.execute(query)
+#        conn.commit()
 
 
 #print(crimeArray[1][1])
