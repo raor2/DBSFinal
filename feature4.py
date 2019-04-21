82a63588dbe35077e3b4f8e6af2b32c605c4b7f7
southwick-dan <daniel.southwick@outlook.com>
2019-04-21 13:32:16 -0400

    Update Feature 4

--- feature4.py
+++ feature4.py
@@ -1,57 +1,68 @@
 import psycopg2
 import psycopg2.extras
 import matplotlib.pyplot as plt
-import numpy as np
-#Outputs the graph of crime rate vs employment 
-#based on the region ( County)
+from matplotlib.ticker import MaxNLocator
 
+# Connect to database
+connection_string = "host='localhost' dbname='finalproject' user='finalproject' password='finalproject'"
+conn = psycopg2.connect(connection_string, cursor_factory=psycopg2.extras.DictCursor)
+cursor = conn.cursor()
 
+# Option for display available areas
+inp = input("Would you like to see a list of available areas?\n\t1. Yes\n\t2. No\n")
+if(int(inp) == 1):
+    print("List of Eligible Areas:\n")
+    cursor.execute("SELECT DISTINCT area FROM arealookup;")
+    data = cursor.fetchall()
+    for row in data:
+        print(row[0])
 
+# Specify area
+area = input("Enter Area:\n")
 
-#def CrimeVSEmployment(InputArea):
+'''
+    Problem 1: Employment data does not have county colunm
+    Problem 2: Empty row of totalcrime means 0
+    Problem 3: Avgemployment usually remains constant while totalcrime decreases
+    Problem 4: Need the working age population to calculate Employment Rate；
+               Need total population to calculate Crime Rate per 100,000 Population
+'''
+# Query data with specified area
+cursor.execute("SELECT crimes.area, crimes.year, crimes.totalcrimes, wages.numestablishments, wages.avgemployment \
+                FROM (SELECT DISTINCT lookup.area, crime.year, sum(crime.totalcrimes::INT) AS totalcrimes \
+                      FROM (SELECT DISTINCT arealookup.area, countylookup.agency \
+                            FROM arealookup, countylookup \
+                            WHERE arealookup.area = %s AND arealookup.county = countylookup.county) AS lookup, crime \
+                      WHERE lookup.agency = crime.agency AND crime.agency <> 'County Total' AND crime.totalcrimes <> '' AND lookup.area <> '' \
+                      GROUP BY lookup.area, crime.year) AS crimes, wages \
+                WHERE crimes.area = wages.area AND crimes.year = wages.year AND wages.naics = '0' \
+                ORDER BY crimes.year ASC;", [area])
+records = cursor.fetchall()
 
-def CrimeAndEmployment(InputCounty):
-    connection_string = "host='localhost' dbname='finalproject' user='finalproject' password='finalproject'"
-    conn = psycopg2.connect(connection_string, cursor_factory=psycopg2.extras.DictCursor)
-    cursor = conn.cursor()
-    cursor.execute("SELECT county FROM AreaLookup;")	
-    county = cursor.fetchall()
-    assert ([InputCounty] in county)
-    #FIND employment 
-    cursor.execute("SELECT W.year, SUM(CAST (W.avgEmployment AS int))\
-    		FROM AreaLookup A, Wages W \
-    		WHERE A.county = %s \
-    			AND A.area = W.area \
-    		GROUP BY W.year	\
-            ORDER BY W.year ASC",(InputCounty,))
-    employment = cursor.fetchall()
-    employment = np.array(employment)
-    #FINF crime
-    cursor.execute("SELECT c.year, SUM( CAST(c.totalcrimes AS int))\
-        FROM data.countylookup cl, data.crime c\
-        WHERE cl.county=%s \
-        	AND cl.agency = c.agency\
-        	AND c.totalcrimes <> ''\
-        GROUP BY c.year\
-        ORDER BY c.year ASC",(InputCounty,))
-    crime = cursor.fetchall()
-    crime = np.array(crime)
-    #PLOT
-    
-    x = crime[:,0] 
-    y = crime[:,1]
-    plt.plot(x, y) 
-    plt.xlabel('x - axis') 
-    plt.ylabel('y - axis') 
-    plt.title('crime') 
-    plt.rcParams["figure.figsize"] = [16,9]
-    plt.show() 
-    
-    x = employment[:,0] 
-    y = employment[:,1]
-    plt.plot(x, y) 
-    plt.xlabel('x - axis') 
-    plt.ylabel('y - axis') 
-    plt.title('employment') 
-    plt.show() 
-    
+# Parse query result
+year, crime, establishment, employment = [], [], [], []
+for row in records:
+    year.append(int(row[1]))
+    crime.append(int(row[2]))
+    establishment.append(int(row[3]))
+    employment.append(int(row[4]))
+employment_per_establishment = [e1/e2 for (e1, e2) in zip(employment, establishment)]
+
+# Normalize data for drawing
+max_crime = max(crime)
+crime = [c/max_crime for c in crime]
+max_establishment = max(establishment) * len(crime) / sum(crime) # max(establishment)/avg(crime)
+establishment = [e/max_establishment for e in establishment]
+max_epe = max(employment_per_establishment) * len(crime) / sum(crime)
+employment_per_establishment = [epe/max_epe for epe in employment_per_establishment]
+
+# Draw graph
+plt.plot(year, crime, label='Crime Rate')
+plt.plot(year, establishment, label='Establishment Number')
+plt.plot(year, employment_per_establishment, label='Employment Per Establishment')
+plt.title('Crime Rate vs. Employment Index')
+plt.xlabel('Year')
+plt.gca().xaxis.set_major_locator(MaxNLocator(integer=True))
+plt.ylabel('Index')
+plt.legend()
+plt.show()
--- feature5.py
+++ feature5.py
@@ -1,65 +1,65 @@
 import psycopg2
 import psycopg2.extras
 
-# Check the most recent crime, employment ratio to decide the bset county to live in
-connection_string = "host='localhost' dbname='finalproject' user='finalproject' password='finalproject'"
-conn = psycopg2.connect(connection_string, cursor_factory=psycopg2.extras.DictCursor)
-cursor = conn.cursor()
-cursor.execute("SELECT a.county, b.avgcrimes, a.avgemployment, a.year \
-FROM  \
-(SELECT DISTINCT county, SUM(CAST(avgemployment as INT)) as avgEmployment, year \
-FROM arealookup LEFT JOIN wages ON arealookup.area = wages.area \
-WHERE year = '2017' \
-GROUP BY (county,year) \
-) a , \
-(SELECT DISTINCT county, SUM(CAST(totalCrimes as INT)) as avgCrimes,year \
- FROM CountyLookup LEFT JOIN crime \
- ON CountyLookup.Agency = crime.Agency \
-WHERE year = '2017' \
-GROUP BY (county,year) \
-) b \
-WHERE a.county = b.county \
-ORDER BY county ASC;")
-
-records = cursor.fetchall()
-
-# crime_employment_data in format: 'county' : (avg,crimes,avgemployment)
-crime_employment_data = {}
-for row in records:
-    crime_employment_data[row[0]] = (float(row[1]),float(row[2]))
-
-print("BEST NY COUNTIES TO PLACE YOUR RESIDENCY IN")
-mult1 = raw_input("I. On a scale of 1-10, How much do you care about your future county's employment rate: \n")
-while not mult1.isdigit():
-    mult1 = raw_input("Input must be an integer from 1 to 10, please re-enter: \n")
-
-while not ((int(mult1) > 0) and (int(mult1) < 11)):
-    mult1 = raw_input("Input must be an integer from 1 to 10, please re-enter: \n")
-        
-mult2 = raw_input("II. On a scale of 1-10, How much do you care about your future county's safety: \n")
-while not mult2.isdigit():
-    mult2 = raw_input("Input must be an integer from 1 to 10, please re-enter: \n")
-
-while not ((int(mult2) > 0) and (int(mult2) < 11)):
-    mult2 = raw_input("Input must be an integer from 1 to 10, please re-enter: \n")
-
-
-best_county = []
-for county in crime_employment_data:
-    ratio = float(mult1)*crime_employment_data[county][1]/10 + (1-float(mult2))*crime_employment_data[county][1]/10
-    best_county.append((county, 2*ratio/(crime_employment_data[county][1] + crime_employment_data[county][0])))
-
-best_county = sorted(best_county, key = lambda x: x[1], reverse = True)
-
-print("")
-print("=============================================")
-print("TOP 10 NY COUNTIES TO PLACE YOUR RESIDENCY IN")
-print("=============================================")
-i = 1
-for i in range(1,10):
-    print(i, best_county[i-1][0])
-    i += 1
-
+def TopCounties():
+    # Check the most recent crime, employment ratio to decide the bset county to live in
+    connection_string = "host='localhost' dbname='finalproject' user='finalproject' password='finalproject'"
+    conn = psycopg2.connect(connection_string, cursor_factory=psycopg2.extras.DictCursor)
+    cursor = conn.cursor()
+    cursor.execute("SELECT a.county, b.avgcrimes, a.avgemployment, a.year \
+    FROM  \
+    (SELECT DISTINCT county, SUM(CAST(avgemployment as INT)) as avgEmployment, year \
+    FROM arealookup LEFT JOIN wages ON arealookup.area = wages.area \
+    WHERE year = '2017' \
+    GROUP BY (county,year) \
+    ) a , \
+    (SELECT DISTINCT county, SUM(CAST(totalCrimes as INT)) as avgCrimes,year \
+     FROM CountyLookup LEFT JOIN crime \
+     ON CountyLookup.Agency = crime.Agency \
+    WHERE year = '2017' \
+    GROUP BY (county,year) \
+    ) b \
+    WHERE a.county = b.county \
+    ORDER BY county ASC;")
+    
+    records = cursor.fetchall()
+    
+    # crime_employment_data in format: 'county' : (avg,crimes,avgemployment)
+    crime_employment_data = {}
+    for row in records:
+        crime_employment_data[row[0]] = (float(row[1]),float(row[2]))
+    
+    print("BEST NY COUNTIES TO PLACE YOUR RESIDENCY IN")
+    mult1 = raw_input("I. On a scale of 1-10, How much do you care about your future county's employment rate: \n")
+    while not mult1.isdigit():
+        mult1 = raw_input("Input must be an integer from 1 to 10, please re-enter: \n")
+    
+    while not ((int(mult1) > 0) and (int(mult1) < 11)):
+        mult1 = raw_input("Input must be an integer from 1 to 10, please re-enter: \n")
+            
+    mult2 = raw_input("II. On a scale of 1-10, How much do you care about your future county's safety: \n")
+    while not mult2.isdigit():
+        mult2 = raw_input("Input must be an integer from 1 to 10, please re-enter: \n")
+    
+    while not ((int(mult2) > 0) and (int(mult2) < 11)):
+        mult2 = raw_input("Input must be an integer from 1 to 10, please re-enter: \n")
+    
+    
+    best_county = []
+    for county in crime_employment_data:
+        ratio = float(mult1)*crime_employment_data[county][1]/10 + (1-float(mult2))*crime_employment_data[county][1]/10
+        best_county.append((county, 2*ratio/(crime_employment_data[county][1] + crime_employment_data[county][0])))
+    
+    best_county = sorted(best_county, key = lambda x: x[1], reverse = True)
+    
+    print("")
+    print("=============================================")
+    print("TOP 10 NY COUNTIES TO PLACE YOUR RESIDENCY IN")
+    print("=============================================")
+    i = 1
+    for i in range(1,10):
+        print(i, best_county[i-1][0])
+        i += 1
 
 
 
