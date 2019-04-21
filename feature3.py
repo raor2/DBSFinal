c696946a67bc256b407558e43d965efa64ca8b38
Zhengneng Chen <chenz11@rpi.edu>
2019-04-21 15:39:34 -0400

    Update feature3.py

--- feature3.py
+++ feature3.py
@@ -8,80 +8,62 @@ connection_string = "host='localhost' dbname='finalproject' user='finalproject' 
 conn = psycopg2.connect(connection_string, cursor_factory=psycopg2.extras.DictCursor)
 cursor = conn.cursor()
 
-# Option for choosing area or county
-option1 = input("Enter a selection:\n\t1. Search By Area\n\t2. Search By County\n")
+# Option for displaying available areas
+option1 = input("Would you like to see a list of available areas?\n\t1. Yes\n\t2. No\n")
 print()
 if int(option1) == 1:
-    # Option for displaying available areas
-    option2 = input("Would you like to see a list of available areas?\n\t1. Yes\n\t2. No\n")
+    print("List of Eligible Areas:")
+    cursor.execute("SELECT DISTINCT area FROM arealookup ORDER BY area ASC;")
+    data = cursor.fetchall()
+    for row in data:
+        print(row[0])
     print()
-    if int(option2) == 1:
-        print("List of Eligible Areas:")
-        cursor.execute("SELECT DISTINCT area FROM arealookup ORDER BY area ASC;")
-        data = cursor.fetchall()
-        for row in data:
-            print(row[0])
-        print()
 
-    # Specify area
-    area = input("Enter Area:\n")
+# Specify area
+area = input("Enter Area:\n")
+print()
 
-    # Query data with specified area
-    cursor.execute("SELECT DISTINCT lookup.area, crime.year, sum(crime.totalcrimes::INT) AS totalcrimes \
-                    FROM (SELECT DISTINCT arealookup.area, countylookup.agency \
-                          FROM arealookup, countylookup \
-                          WHERE arealookup.area = %s AND arealookup.county = countylookup.county) AS lookup, crime \
-                    WHERE lookup.agency = crime.agency AND crime.agency <> 'County Total' AND crime.totalcrimes <> '' AND lookup.area <> '' \
-                    GROUP BY lookup.area, crime.year \
-                    ORDER BY crime.year ASC;", [area])
-    records = cursor.fetchall()
-
-    # Parse query result
-    year, crime = [], []
-    for row in records:
-        year.append(int(row[1]))
-        crime.append(int(row[2]))
-
-    # Draw graph
-    plt.plot(year, crime)
-    plt.title('Crime Rate History of {} Area'.format(area))
-    plt.xlabel('Year')
-    plt.ylabel('Total Number of Crimes Reported')
-    plt.show()
-elif int(option1) == 2:
-    # Option for displaying available counties
-    option2 = input("Would you like to see a list of available counties?\n\t1. Yes\n\t2. No\n")
+# Option for displaying available industries
+option2 = input("Would you like to see a list of available industries?\n\t1. Yes\n\t2. No\n")
+print()
+if int(option2) == 1:
+    print("List of Eligible Industries:")
+    cursor.execute("SELECT DISTINCT naics, naicstitle FROM naicslookup ORDER BY naics ASC;")
+    data = cursor.fetchall()
+    for row in data:
+        print("{0:<5s}   {1}".format(row[0], row[1]))
     print()
-    if int(option2) == 1:
-        print("List of Eligible Counties:")
-        cursor.execute("SELECT DISTINCT county from countylookup ORDER BY county ASC;")
-        data = cursor.fetchall()
-        for row in data:
-            print(row[0])
-        print()
 
-    # Specify county
-    county = input("Enter County:\n")
+# Specify area
+naics = input("Enter NAICS:\n")
 
-    # Query data with specified county
-    cursor.execute("SELECT DISTINCT countylookup.county, crime.year, sum(crime.totalcrimes::INT) AS totalcrimes \
-                    FROM countylookup, crime \
-                    WHERE countylookup.county = %s AND countylookup.agency = crime.agency AND crime.agency <> 'County Total' AND crime.totalcrimes <> '' \
-                    GROUP BY countylookup.county, crime.year \
-                    ORDER BY crime.year ASC;", [county])
-    records = cursor.fetchall()
+# Query data with specified area
+cursor.execute("SELECT DISTINCT wages.area, wages.year, naicslookup.naicstitle, wages.numestablishments, wages.avgemployment \
+                FROM wages, naicslookup \
+                WHERE wages.area = %s AND naicslookup.naics = %s AND wages.naics = naicslookup.naics \
+                ORDER BY wages.year ASC;", [area, naics])
+records = cursor.fetchall()
 
-    # Parse query result
-    year, crime = [], []
-    for row in records:
-        year.append(int(row[1]))
-        crime.append(int(row[2]))
-    
-    # Draw graph
-    plt.plot(year, crime)
-    plt.title('Crime Rate History of {} County'.format(county))
-    plt.xlabel('Year')
-    plt.ylabel('Total Number of Crimes Reported')
-    plt.show()
-else:
-    print("Invalid option\n")
+# Parse query result
+year, title, establishment, employment = [], records[0][2], [], []
+for row in records:
+    year.append(int(row[1]))
+    establishment.append(int(row[3]))
+    employment.append(int(row[4]))
+employment_per_establishment = [e1/e2 for (e1, e2) in zip(employment, establishment)]
+
+# Normalize data for drawing
+max_establishment = max(establishment)# max(establishment)/avg(crime)
+establishment = [e/max_establishment for e in establishment]
+max_epe = max(employment_per_establishment) * len(establishment) / sum(establishment)
+employment_per_establishment = [epe/max_epe for epe in employment_per_establishment]
+
+# Draw graph
+plt.plot(year, establishment, label='Establishment Number')
+plt.plot(year, employment_per_establishment, label='Employment Per Establishment')
+plt.title('Employment History of {} Industry in {} Area'.format(title, area))
+plt.xlabel('Year')
+plt.gca().xaxis.set_major_locator(MaxNLocator(integer=True))
+plt.ylabel('Index')
+plt.legend()
+plt.show()
