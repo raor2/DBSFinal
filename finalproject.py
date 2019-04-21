e2babbc17c8f7828b60a80445ff881f3dc829c4b
Ryan Rao <raor2@rpi.edu>
2019-04-21 15:48:32 -0400

    file

--- finalproject.py
+++ finalproject.py
@@ -16,13 +16,16 @@ while(1):
         print("List of Available Features:\n\t1. Explore Crime Data by Area and by County\n\t2. Explore New York Industry Data by Area\n\t3. Visualize Employment and Crime Data For a Given Area\n\t4. Prioritized Living Area R   ecommendation Based On Crime and Employment ")
     if(choice == '1'):
         print("Feature 1")
-        exec(open('RawDataLookup.py').read())
+        exec(open('feature1.py').read())
     if(choice == '2'):
         print("Feature 2")
+        exec(open('feature3.py').read())
     if(choice == '3'):
         print("Feature 3")
+        exec(open('feature4.py').read())
     if(choice == '4'):
         print("Feature 4")
+        exec(open('feature5.py').read())
 
 
 print("End program")
