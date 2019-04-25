2c4d54967ad6e53cd674560824933a7fec00c5b7
Ryan Rao <raor2@rpi.edu>
2019-04-25 16:34:34 -0400

    Added load data call

--- application.py
+++ application.py
@@ -3,9 +3,13 @@ title = "\n\n\t\t\t\tExploration of Crime and Employment Data for New York"
 
 title = title.center(24)
 
+print("Loading Data...")
+exec(open('load_data.py').read())
+print("Data Loaded")
 print(title)
 print("Team Members:\nYang Zhang\nDan Southwick\nZhengneng Chen\nRyan Rao\n\n") #Everyone else add your names here
-print("List of Available Features:\n\t1. Explore Crime Data by Area and by County\n\t2. Explore New York Industry Data by Area\n\t3. Visualize Employment and Crime Data For a Given Area\n\t4. Prioritized Living Area R   ecommendation Based On Crime and Employment")
+
+print("List of Available Features:\n\t1. Explore Crime Data by Area and by County\n\t2. Explore New York Industry Data by Area\n\t3. Visualize Employment and Crime Data For a Given Area\n\t4. Prioritized Living Area Recommendation Based On Crime and Employment")
 
 while(1):
     choice = input("\nEnter a feature choice, q to quit or f to see the features again:\n")
